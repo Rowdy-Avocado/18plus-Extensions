375c30293c6404823b5133df13c13f2dcd858cd1
Salman Ilyas <salmanilyas731@gmail.com>
2024-10-05 22:38:04 +0500

    add stripchat and chatrubate live providers

---
+++ Chatrubate/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("luck731")
+    language    = "en"
+    description = "Chaturbate - Free Adult Webcams, Live Sex, Free Sex Chat, Exhibitionist & Pornstar Free Cams"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+    iconUrl = "https://web.static.mmcdn.com/images/logo.svg?hash=7c993ccd600a"
+}
