1322364caf0cd6e4fae081605c1e72af8c193208
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2024-10-16 17:01:20 +0530

    Update TollyPro.kt

--- TollyPro/src/main/kotlin/com/TollyPro/TollyPro.kt
+++ TollyPro/src/main/kotlin/com/TollyPro/TollyPro.kt
@@ -16,7 +16,7 @@ import org.jsoup.nodes.Element
 
 class TollyPro : MainAPI() {
 
-    override var mainUrl = "https://tellyhd.sbs"
+    override var mainUrl = "https://tellyhd.skin"
     override var name = "TellyHD"
     override val hasMainPage= true
     override var lang= "hi"
